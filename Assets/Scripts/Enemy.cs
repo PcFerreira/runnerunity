9715bdda2953bee498bc99d073b561bf88a4c897
Pedro Ferreira <cesar_pedro@outlook.com>
2020-12-17 22:40:55 +0300

    Atividade 17-12

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,35 +4,43 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-
+    
     public float Speed;
-    public Transform backPoint;
+    private Transform backPoint;
+
     private Animator animator;
     private Rigidbody2D rig;
 
-    void Start() {
-       backPoint = GameObject.Find("backPoint").GetComponent<Transform>(); 
-       animator = GetComponent<Animator>();
-       rig = GetComponent<Rigidbody2D>();
+    void Start()
+    {
+        backPoint = GameObject.Find("backPoint").GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        //transform.Translate(Vector3.left * Speed * Time.deltaTime);
-        
+        //Mesma logica da bala só que para a esquerda
+        //transform.Translate(Vector3.left * Speed * Time.deltaTime); 
+
         rig.velocity = new Vector2(-Speed, rig.velocity.y);
 
-        if(transform.position.x < backPoint.position.x){
+        if(transform.position.x < backPoint.position.x)      
+        {
             Destroy(gameObject);
         }
-
     }
 
-    void OnTriggerEnter2D(Collider2D collision) {
-        if(collision.gameObject.tag == "Bullet"){
+    //Metodo para verificar colisao da bala no inimigo
+    void OnTriggerEnter2D(Collider2D collision) 
+    {
+        //Se o inimigo bateu na bala
+        if(collision.gameObject.tag == "bullet")
+        {
             animator.SetTrigger("destroy");
             Destroy(gameObject, 1f);
         }
+        
     }
 }
--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -4,17 +4,14 @@ using UnityEngine;
 
 public class SpawnEnemies : MonoBehaviour
 {
-
+    
     public float initialTime;
     public float minTime;
     public float maxTime;
-    
+
     public List<GameObject> EnemiesList = new List<GameObject>();
-    
 
     float timeControl;
-
-
 
     void Update()
     {
@@ -23,10 +20,11 @@ public class SpawnEnemies : MonoBehaviour
         if(timeControl >= initialTime)
         {
             Instantiate(EnemiesList[Random.Range(0, EnemiesList.Count)], 
-            transform.position + new Vector3(0, Random.Range(-2, 3), 0), transform.rotation);
+                transform.position + new Vector3(0, Random.Range(-2,2), 0), transform.rotation);
 
             initialTime = Random.Range(minTime, maxTime);
             timeControl = 0;
         }
+        
     }
 }
